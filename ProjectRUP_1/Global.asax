36687ac18a53d4180a08cfa2ecec23d6ef625910
wasgoed <wasgoed@live.nl>
2015-03-19 14:27:51 +0100

    19-3

---
+++ ProjectRUP_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectRUP_1.MvcApplication" Language="C#" %>
---
+++ ProjectRUP_1/Models/ExamsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectRUP_1.Models
+{
+    public class ExamsController : Controller
+    {
+        private HUInspectorEntities db = new HUInspectorEntities();
+
+        // GET: Exams
+        public ActionResult Index()
+        {
+            return View(db.Exam.ToList());
+        }
+
+        // GET: Exams/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exam exam = db.Exam.Find(id);
+            if (exam == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exam);
+        }
+
+        // GET: Exams/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Exams/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Creator,Speciality,ComputerNeeded,Surveillant,ExpectedStudents,ResultIsDecimal,QuarterId,MinutesDuration")] Exam exam)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Exam.Add(exam);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(exam);
+        }
+
+        // GET: Exams/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exam exam = db.Exam.Find(id);
+            if (exam == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exam);
+        }
+
+        // POST: Exams/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Creator,Speciality,ComputerNeeded,Surveillant,ExpectedStudents,ResultIsDecimal,QuarterId,MinutesDuration")] Exam exam)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(exam).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(exam);
+        }
+
+        // GET: Exams/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exam exam = db.Exam.Find(id);
+            if (exam == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exam);
+        }
+
+        // POST: Exams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Exam exam = db.Exam.Find(id);
+            db.Exam.Remove(exam);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
